ca38d3a8851978399b3bd125dad7e2965c3d731b
Dusan Malusev <dusan@dusanmalusev.dev>
2024-03-05 12:27:31 +0100

    feat: trait arch tests
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- tests/Architecture/TraitTest.php
+++ tests/Architecture/TraitTest.php
@@ -6,3 +6,11 @@ arch('traits')
     ->expect('CodeLieutenant\LaravelCrypto\Traits')
     ->toBeTraits();
 
+arch('hashing traits')
+    ->expect('CodeLieutenant\LaravelCrypto\Hashing\Traits')
+    ->toBeTraits();
+
+arch('signing traits')
+    ->expect('CodeLieutenant\LaravelCrypto\Signing\Traits')
+    ->toBeTraits();
+
