287d3d0f7a21f3335534eddfb08741bdb7033f7e
Dusan Malusev <dusan@dusanmalusev.dev>
2024-03-05 12:33:53 +0100

    feat: add keys arch tests
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

---
+++ tests/Architecture/KeysTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use CodeLieutenant\LaravelCrypto\Contracts\KeyLoader;
+
+arch('keys')
+    ->expect('CodeLieutenant\LaravelCrypto\Keys')
+    ->toBeClasses()
+    ->toImplement(KeyLoader::class);
